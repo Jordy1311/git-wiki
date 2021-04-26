b3de86304821ed2fc7636e2a231ece4e655b7510
Jordy1311 <jordancooper1195@gmail.com>
2021-04-26 17:52:35 +1200

    page frame & headings

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-window.onload = function() {
-    console.log("testing .js file is loaded!")
-}
+// SCROLLSPY
+$('body').scrollspy({target: '#main-nav'});
+
+// ANIMATED SCROLLING FOR SMOOTH SCROLLING
+$('#main-nav a').on('click', function(e) {
+    // CHECK FOR A HASH VALUE
+    if(this.hash !== '') {
+    // PREVENT DEFAULT BEHAVIOUR
+    e.preventDefault();
+
+    // STORE HASH
+    const hash = this.hash;
+
+    // ANIMATE SMOOTH SCROLL
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 500, function() {
+        // ADD HASH TO URL AFTER SCROLL
+        window.location.hash = hash;
+    });
+    }
+});
