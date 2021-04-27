4ec573a4048129521d5d8535dcfe549c6503a048
Jordy1311 <jordancooper1195@gmail.com>
2021-04-27 13:32:50 +1200

    polishing layout & navbar

--- app.js
+++ app.js
@@ -2,21 +2,16 @@
 $('body').scrollspy({target: '#main-nav'});
 
 // ANIMATED SCROLLING FOR SMOOTH SCROLLING
-$('#main-nav a').on('click', function(e) {
-    // CHECK FOR A HASH VALUE
-    if(this.hash !== '') {
-    // PREVENT DEFAULT BEHAVIOUR
-    e.preventDefault();
+$('#main-nav a').on('click', function(event) {
+    if (this.hash !== "") {
+    event.preventDefault();
 
-    // STORE HASH
     const hash = this.hash;
 
-    // ANIMATE SMOOTH SCROLL
     $('html, body').animate({
         scrollTop: $(hash).offset().top
-    }, 500, function() {
-        // ADD HASH TO URL AFTER SCROLL
+    }, 300, function() {
         window.location.hash = hash;
-    });
+        });
     }
 });
