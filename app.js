adbf2b9370938c9a5911fac775cdc3d78a7483ef
Jordy1311 <jordancooper1195@gmail.com>
2021-04-28 20:00:10 +1200

    added/amended content & eventlistening for diagram

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// GET ELEMENTS
+let diagramTextArea = document.getElementById('workings-description')
+
+let headingMouseOver = function(e) {
+    if(e.target.id.includes('heading')) {
+        e.target.style.color = '#FFD300';
+    }
+}
+
+let headingMouseOut = function(e) {
+    if(e.target.id.includes('heading')) {
+        setTimeout(function() {e.target.style.color = '#000';}, 700)
+    }
+}
+
+diagramTextArea.addEventListener('mouseover', headingMouseOver);
+diagramTextArea.addEventListener('mouseout', headingMouseOut);
+
 // SCROLLSPY
 $('body').scrollspy({target: '#main-nav'});
 
