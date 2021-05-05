9ae8d2bb59d0622211e56fc8ddca1a145b136320
Jordy1311 <jordancooper1195@gmail.com>
2021-05-05 15:34:06 +1200

    changed to css smoothscroll

--- app.js
+++ app.js
@@ -18,18 +18,3 @@ diagramTextArea.addEventListener('mouseout', headingMouseOut);
 
 // SCROLLSPY
 $('body').scrollspy({target: '#main-nav'});
-
-// ANIMATED SCROLLING FOR SMOOTH SCROLLING
-$('#main-nav a').on('click', function(event) {
-    if (this.hash !== "") {
-    event.preventDefault();
-
-    const hash = this.hash;
-
-    $('html, body').animate({
-        scrollTop: $(hash).offset().top
-    }, 300, function() {
-        window.location.hash = hash;
-        });
-    }
-});
