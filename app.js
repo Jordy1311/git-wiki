784a62e0c76f77d71bbf0715c15e333be1d96c21
Jordy1311 <jordancooper1195@gmail.com>
2021-05-14 13:42:23 +1200

    fixed workingsDiagram

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 // DIAGRAM TYPEWRITER EFFECT
-let diagramTextArea = document.getElementById('workings-description')
-let workingsDiagram = document.getElementById('workings-diagram-text')
+let workingsTextArea = document.getElementById('workings-description');
+let workingsDiagram = document.getElementById('workings-diagram-text');
 
-let headingMouseOver = (e) => {
-    if(e.target.id.includes('heading')) {
-        workingsDiagram.textContent += e.target.textContent
+let headingMouseOver = (event) => {
+    if(event.target.id.includes('heading')) {
+        let initial = 'computer:';
+        let heading = event.target.id;
+        switch (heading) {
+            case 'main-heading':
+                workingsDiagram.textContent = `${initial} git branch \nOn branch main...`
+                break;
+            case 'pulling-heading':
+                workingsDiagram.textContent = `${initial} git pull \nPulling...`
+                break;
+            case 'branches-heading':
+                workingsDiagram.textContent = `${initial} git branch \nOn branch main...`
+                break;
+            case 'tracked-heading':
+                workingsDiagram.textContent = `${initial} git status \nOn branch main...\n Chanes not staged for commit... - X\n-Y\n-Z`
+                break;
+            case 'staging-heading':
+                workingsDiagram.textContent = `${initial} git add . \nFILES ADDED...`
+                break;
+        }
+        
+        
+        workingsDiagram.textContent += ` ${event.target.textContent}`;
     }
 }
 
-let headingMouseOut = (e) => {
-    if(e.target.id.includes('heading')) {
-        workingsDiagram.textContent = 'computer:~'
+let headingMouseOut = (event) => {
+    if(event.target.id.includes('heading')) {
+        workingsDiagram.textContent = 'computer:~';
     }
 }
 
-diagramTextArea.addEventListener('mouseover', headingMouseOver);
-diagramTextArea.addEventListener('mouseout', headingMouseOut);
+workingsTextArea.addEventListener('mouseover', headingMouseOver);
+workingsTextArea.addEventListener('mouseout', headingMouseOut);
